6109abeb993334326105d2971563c2c649bf6617
Groovy52 <78290328+Groovy52@users.noreply.github.com>
2026-02-27 17:17:15 +0000

    Deploying to gh-pages from @ Groovy52/Groovy52.github.io@134c06b9cf839c464a4763daa9685ac74447e3fe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -472,11 +472,6 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
             },},{id: "projects-sald-net",
           title: 'SALD-Net',
           description: "Self-attention-integrated LiDAR-based 3D object detection network in a crowded hospital environment",
